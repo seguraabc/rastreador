08ce8549cb06008a50f08ced15b36a0d97ec8d75
seguraabc <seguraabc@gmail.com>
2025-11-22 00:58:13 -0300

    feat: Add Capacitor Android platform with initial project structure, resources, and build files.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.segura.rastreador',
+  appName: 'rastreador',
+  webDir: 'dist'
+};
+
+export default config;
